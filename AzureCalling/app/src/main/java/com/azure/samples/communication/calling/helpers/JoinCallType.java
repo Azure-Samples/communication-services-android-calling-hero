27a9fb764c420703777d182f52de0fb313fdaff3
Inderpal Singh Aulakh <73618019+iaulakh@users.noreply.github.com>
2021-04-07 16:32:32 -0700

    Refactor Parameters for Joining Calls (#19)
    * rename group id to join Id

---
+++ AzureCalling/app/src/main/java/com/azure/samples/communication/calling/helpers/JoinCallType.java
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.samples.communication.calling.helpers;
+
+public enum JoinCallType {
+    GROUP_CALL
+}
