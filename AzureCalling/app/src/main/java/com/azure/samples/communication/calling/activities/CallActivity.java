68a7f1fc57bf485e1607cfb23a233d02865afc12
Mrayyan <t-musiddiqui@microsoft.com>
2022-03-08 13:23:44 -0800

    [Feature] UI Support for small screen devices (#108)

--- AzureCalling/app/src/main/java/com/azure/samples/communication/calling/activities/CallActivity.java
+++ AzureCalling/app/src/main/java/com/azure/samples/communication/calling/activities/CallActivity.java
@@ -104,7 +104,8 @@ public class CallActivity extends AppCompatActivity {
         @Override
         public void accept(final AudioDeviceType audioDeviceType) {
             final int drawableId = audioDeviceType == AudioDeviceType.ANDROID
-                    ? R.drawable.ic_fluent_speaker_2_28_regular : R.drawable.ic_fluent_speaker_2_28_filled;
+                    ? R.drawable.ic_fluent_speaker_2_white_24_regular
+                    : R.drawable.ic_fluent_speaker_2_white_24_filled;
             setDrawable(deviceOptionsButton, drawableId);
         }
     };
@@ -770,7 +771,7 @@ public class CallActivity extends AppCompatActivity {
         callHangupCancelButton.setOnClickListener(l -> closeHangupDialog());
 
         deviceOptionsButton = findViewById(R.id.audio_device_button);
-        deviceOptionsButton.setTag(R.drawable.ic_fluent_speaker_2_28_regular);
+        deviceOptionsButton.setTag(R.drawable.ic_fluent_speaker_2_white_24_regular);
         deviceOptionsButton.setOnClickListener(l -> openAudioDeviceList());
 
         infoHeaderView = findViewById(R.id.info_header);
