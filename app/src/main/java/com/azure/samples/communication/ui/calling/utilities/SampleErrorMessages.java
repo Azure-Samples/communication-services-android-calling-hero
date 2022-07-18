7cc476913b64c59409134bfbb3ced86000583434
Mohtasim <mbellah@microsoft.com>
2022-07-18 16:41:10 -0700

    Adding sample error messages
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

---
+++ app/src/main/java/com/azure/samples/communication/ui/calling/utilities/SampleErrorMessages.java
@@ -0,0 +1,5 @@
+package com.azure.samples.communication.ui.calling.utilities;
+
+public class SampleErrorMessages {
+    public static final String GROUP_ID_ERROR_MESSAGE = "Group call ID is required";
+}
