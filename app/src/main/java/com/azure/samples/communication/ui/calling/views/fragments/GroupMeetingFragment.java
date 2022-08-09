4aad4c6d543bf2f921531608ca6db14df4f86908
Dhiraj Gupta <dhiraj@traversient.com>
2022-08-09 16:55:57 -0400

    Fixed crash with absence of no-params constructor in GroupMeetingFragment

--- app/src/main/java/com/azure/samples/communication/ui/calling/views/fragments/GroupMeetingFragment.java
+++ app/src/main/java/com/azure/samples/communication/ui/calling/views/fragments/GroupMeetingFragment.java
@@ -23,8 +23,6 @@ import com.microsoft.fluentui.widget.Button;
  */
 public class GroupMeetingFragment extends Fragment {
 
-    Context context;
-
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -41,10 +39,7 @@ public class GroupMeetingFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public GroupMeetingFragment(final Context context) {
-        // Required empty public constructor
-        this.context = context;
-    }
+    public GroupMeetingFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
