7c20c72dc914fc9a5cdb537d74b5fdf7d1ba3122
Mohtasim <mbellah@microsoft.com>
2022-08-10 15:07:57 -0700

    MSAL Auth implementation
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

--- app/src/main/java/com/azure/samples/communication/ui/calling/views/activities/SignInActivity.java
+++ app/src/main/java/com/azure/samples/communication/ui/calling/views/activities/SignInActivity.java
@@ -4,25 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.azure.samples.communication.ui.calling.AzureUICalling;
 import com.azure.samples.communication.ui.calling.R;
+import com.azure.samples.communication.ui.calling.externals.authentication.AADAuthHandler;
+import com.azure.samples.communication.ui.calling.utilities.AppSettings;
 import com.azure.samples.communication.ui.calling.views.activities.IntroViewActivity;
 import com.microsoft.fluentui.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
 
     private Button signInButton;
+    private AADAuthHandler authHandler;
+    private AppSettings appSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
+
+
+        initializeAuth();
+        initializeUI();
+    }
+
+    private void initializeUI() {
         signInButton = findViewById(R.id.sign_in_button);
         signInButton.setOnClickListener(l -> navigateToIntroViewPage());
     }
 
+    private void initializeAuth() {
+        authHandler = ((AzureUICalling) getApplication()).getAadAuthHandler();
+        appSettings = ((AzureUICalling) getApplication()).getAppSettings();
+    }
+
     private void navigateToIntroViewPage() {
+
+        if (appSettings.isAADAuthEnabled()) {
+            authHandler.loadAccount(this, (isAccountFound) -> {
+                if (!isAccountFound) {
+                  authHandler.signIn(this, () -> {
+                      Log.d("Mohtasim", "Signed in");
+                  });
+                }
+            });
+        }
         final Intent intent = new Intent(this, IntroViewActivity.class);
         startActivity(intent);
     }
