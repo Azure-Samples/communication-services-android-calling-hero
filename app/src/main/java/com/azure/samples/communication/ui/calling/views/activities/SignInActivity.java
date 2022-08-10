e70336d5448677031bdcd70a7a0e3bdfcf5f3967
Dhiraj Gupta <dhirajgupta@dkgTEKLaptop.local>
2022-08-10 19:57:53 -0400

    Showing progress spinner UI while AAD is checking account
    commit 2d3759576354e74c48ff64dcb047be215aeb94ea
    Author: Dhiraj Gupta <dhirajgupta@dkgTEKLaptop.local>
    Date:   Wed Aug 10 19:56:19 2022 -0400
    
        fixed redirect_uri to working
    
    commit 71a63f8bb39cd29df8eb2d19faf771bb2d5096a1
    Merge: 9e29fc1 6bba489
    Author: Dhiraj Gupta <v-dhigupta@microsoft.com>
    Date:   Wed Aug 10 19:36:19 2022 -0400
    
        Merge branch 'compositesample/develop' into compositesample/dhiraj-ui-fixes
    
        # Conflicts:
        #	app/src/main/java/com/azure/samples/communication/ui/calling/views/activities/SignInActivity.java
    
    commit 9e29fc128dd91e9f32b772f198cd859cfaf0d3b3
    Author: Dhiraj Gupta <v-dhigupta@microsoft.com>
    Date:   Wed Aug 10 17:41:58 2022 -0400
    
        Dummy UI for showing progress spinner while signing in with AAD
    
    commit 7d41a115c407d3011af21af0fe561087261e92f0
    Author: Dhiraj Gupta <v-dhigupta@microsoft.com>
    Date:   Wed Aug 10 16:55:50 2022 -0400
    
        refactor: removed warnings
    
    commit 978c1d05c0ff22f0a0735c2566e9f12d5164dbea
    Author: Dhiraj Gupta <v-dhigupta@microsoft.com>
    Date:   Wed Aug 10 16:55:39 2022 -0400
    
        Fixed wrong initials showing in avatar view
    
    commit 017af7d5ad72a4826b329269561181af7aaddc73
    Author: Dhiraj Gupta <v-dhigupta@microsoft.com>
    Date:   Wed Aug 10 14:20:49 2022 -0400
    
        Updated comment on GroupMeetingFragment

--- app/src/main/java/com/azure/samples/communication/ui/calling/views/activities/SignInActivity.java
+++ app/src/main/java/com/azure/samples/communication/ui/calling/views/activities/SignInActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.util.Log;
 
 import com.azure.samples.communication.ui.calling.AzureUICalling;
@@ -41,9 +42,11 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     private void navigateToIntroViewPage() {
-
         if (appSettings.isAADAuthEnabled()) {
+            View progressOverlay = findViewById(R.id.overlay_loading);
+            progressOverlay.setVisibility(View.VISIBLE);
             authHandler.loadAccount(this, (isAccountFound) -> {
+                progressOverlay.setVisibility(View.GONE);
                 if (!isAccountFound) {
                   authHandler.signIn(this, () -> {
                       Log.d("Mohtasim", "Signed in");
@@ -57,6 +60,5 @@ public class SignInActivity extends AppCompatActivity {
                 }
             });
         }
-
     }
 }
